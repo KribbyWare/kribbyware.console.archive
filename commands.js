57d22973d91392d107d103bd0330d49ab97d0c8a
KribbyWare <162795537+KribbyWare@users.noreply.github.com>
2024-08-05 18:28:23 +0200

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,15 @@
+var discord = "https://discord.com/invite/pd9GtP9Cvk";
+var download = "https://github.com/KribbyWare/kribbyware.console.archive/raw/main/AstroX%20Bootstrapper.exe";
+
+help = [
+  "<br>",
+  '<span class="command">download</span>       Downloads AstroX Bootstrapper',
+  '<span class="command">discord</span>        Redirct to Kribby Discord',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Welcome to kribbyware.console</span>',
+  '<span class="colornew">-----------------------------</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
